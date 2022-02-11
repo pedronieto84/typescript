d8e328860463ee26d1952644704360f77cfbb016
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-11 23:30:02 +0100

    Creo archivo index.html para poder ver los console logs en el navegador. Y ejemplo de Template Literals.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hola mundo');
+// Template literals
+var nombre = "Juan";
+var apellido = "Perez";
+var edad = 32;
+// Concatenación de strings con typescript
+var texto = "Hola, ".concat(nombre, " ").concat(apellido, " (").concat(edad, ")");
+console.log(texto);
+// Esto loguea en el navegador
+// Hola Juan Perez, cómo estás
--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
-let spiderman = {
-    nombre: "Peter",
-    edad: 20
-}
+// Template literals
 
-spiderman = {
-    
-}
+let nombre: string = "Juan"
+let apellido: string = "Perez"
+let edad: number = 32
+
+// Concatenación de strings con typescript
+
+let texto = `Hola, ${nombre} ${apellido} (${edad})`
+
+console.log(texto)
+
+// Esto loguea en el navegador
+
+// Hola Juan Perez, cómo estás
