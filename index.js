58895ce4100e17ae148f3981789fd9767746373a
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-12 00:31:25 +0100

    Constructor de una clase

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var numeros = [10, 4, 7, 15, 3, 25];
-console.log(numeros); // Imprime un array.
-console.log.apply(// Imprime un array.
-console, numeros);
+var Ciudad = /** @class */ (function () {
+    function Ciudad(nombre, poblacion) {
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+    }
+    return Ciudad;
+}());
+var ciudad = new Ciudad('Almeria', 196000);
+console.log(ciudad);
--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
 class Ciudad {
-    public nombre: string = 'Almeria'
-    private poblacion: number = 197000
+    public nombre: string 
+    private poblacion: number 
     extension: number
 
-    logName(){
-        console.log("La ciudad es ", this.nombre);
+
+    // EL constructor se llama automaticamente siempre
+    // que se instancia la clase.
+    constructor(nombre: string, poblacion: number){
+        this.nombre = nombre
+        this.poblacion = poblacion
     }
 }
 
 
-let ciudad = new Ciudad()
+let ciudad2 = new Ciudad('Almeria', 196000)
 
 
-ciudad.
+console.log(ciudad2);
+// Esto loguea {nombre: "Almeria", poblacion: 196000}
