d824a5d7357b52a242aec991fc2d6a9401a58fbf
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-11 23:53:09 +0100

    Ejemplos de la nueva sintaxis de arrow functions.

--- index.ts
+++ index.ts
@@ -1,8 +1,25 @@
-// Parámetros opcionales siempre deben ir al final o dará error.
 
-function hola(nombre?, apellido){
-
+const func1 = function (a) {
+    return a
 }
+
+// Esto de abajo es lo mismo que la func1. Menos sintaxis y dará menos errores.
+
+const func2 = (a) => a
+
+/////
+
+const func3 = function( a:number, b:number){
+    return a+b
+}
+
+// La func4 es igual a la 3 y es mejor definirla así por el mismo motivo de antes.
+
+ const functionName = (a:number, b:number) => {
+    return a + b
+};
+
+
 
 
 
