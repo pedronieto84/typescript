58b01098e481fc446e5992c1b370ab53f4566f24
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-12 03:51:10 +0100

    Promise.all

--- main.js
+++ main.js
@@ -12,20 +12,16 @@ const promiseEjemplo = (result, delay) => {
 
 // No importa el orden, ocurren de forma asíncrona y en el then y el catch definimos el callback ( lo que sucederá en cada escenario.)
 (async () => {
-    try {
-        // De esta forma conseguimos hacer que una se ejecute después de la otra
-
-        const primera = await promiseEjemplo(true, 2000)
-        console.log('primera', primera);
-        const segunda = await promiseEjemplo(true, 1000)
-        console.log('segunda', segunda)
-    } catch (error) {
-        // también simplificamos sintaxis agrupando el error 
-        const rej = error;
-        console.error('error', rej)
-    }
-    
-})()
-
-
-
+  try {
+    // De esta forma conseguimos hacer que una se ejecute después de la otra
+    const res = await Promise.all([
+      promiseEjemplo(true, 2000),
+      promiseEjemplo(false, 1000),
+    ]);
+    console.log(res);
+  } catch (error) {
+    // también simplificamos sintaxis agrupando el error
+    const rej = error;
+    console.error("error", rej);
+  }
+})();
