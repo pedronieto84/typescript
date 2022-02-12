6bd0c065e376fe8c94567403d32308eec7b18a87
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-12 03:46:18 +0100

    Async / Await

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-class Animal {
-    constructor(){
-        this.alive = true 
+const promiseEjemplo = (result, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (result) {
+        resolve({ message: "success" });
+      } else {
+        reject({ message: "error" });
+      }
+    }, delay);
+  });
+};
+
+// No importa el orden, ocurren de forma asíncrona y en el then y el catch definimos el callback ( lo que sucederá en cada escenario.)
+(async () => {
+    try {
+        // De esta forma conseguimos hacer que una se ejecute después de la otra
+
+        const primera = await promiseEjemplo(true, 2000)
+        console.log('primera', primera);
+        const segunda = await promiseEjemplo(true, 1000)
+        console.log('segunda', segunda)
+    } catch (error) {
+        // también simplificamos sintaxis agrupando el error 
+        const rej = error;
+        console.error('error', rej)
     }
-
-    come(){
-        console.log("Este animal esta comiendo");
-    }
-}
-
-class Raton extends Animal {
-    corre(){
-        console.log('corre');
-    }
-}
+    
+})()
 
 
 
-const raton = new Raton()
-raton.come()
-raton.corre()
