6a2e7865d57d67d92b68ee0a523d961f6198114b
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-12 02:06:20 +0100

    Generics

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
-
-// Las interfaces son estructuras más complejas ( De echo en mis proyectos con estas ya me basta y me sobra).
-
 interface Persona {
     nombre: string
-    caracteristicas: CaracteristicasFisicas,
-    amigos: Persona
+    caracteristicas?: CaracteristicasFisicas,
+    amigos ?: Persona // el interrogante también aquí sirve para identificar que son opcionales
 }
 
 interface CaracteristicasFisicas {
@@ -12,3 +9,18 @@ interface CaracteristicasFisicas {
     edad: number
     sexo: string | number // por ejemplo, puede ser que 1 sea mujer y 0 sea hombre
 }
+
+
+// Los genericos me sirven para pasarle desde fuera el valor.
+
+// Defino un tipo de función "Si, no lo había mencionado, con typescript también podemos definir tipos de funciones"
+
+function ejemploGenerico <T>(arg: T):T{
+    return arg
+}
+
+const number = ejemploGenerico<number>(1)
+
+const string = ejemploGenerico<string>('asdf')
+
+const Persona = ejemploGenerico<Persona>({ nombre: 'Juan' })
